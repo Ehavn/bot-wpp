07d033586e028b2da33a47110b46e2920e8f34e5
Gilnei Porto <gporto@umov.me>
2025-08-31 22:48:25 -0300

    trabalhando no sanitize, ajustando pastas

---
+++ docker-dataSanitize/src/services/sanitizer.py
@@ -0,0 +1,14 @@
+import re
+
+class Sanitizer:
+    def __init__(self):
+        self.patterns = {
+            "cpf": re.compile(r'\b\d{4,11}\b'),  # pega CPFs escritos de forma errada
+            "email": re.compile(r'\b[\w.-]+@[\w.-]+\.\w{2,}\b'),
+            "phone": re.compile(r'\b\d{2,3}-?\d{4,5}-?\d{4}\b')
+        }
+
+    def sanitize(self, text: str) -> str:
+        for key, pattern in self.patterns.items():
+            text = pattern.sub("***MASKED***", text)
+        return text
---
+++ docker-dataSanitize/src/services/worker_sanitize.py
@@ -0,0 +1,20 @@
+from src.dao.message_dao import MessageDAO
+from src.services.sanitizer import Sanitizer
+from src.utils.mongo_client import get_mongo_client
+from src.utils.logger import get_logger
+
+class WorkerSanitize:
+    def __init__(self):
+        client, config = get_mongo_client()
+        self.dao = MessageDAO(client, config)
+        self.sanitizer = Sanitizer()
+        self.logger = get_logger("WorkerSanitize")
+
+    def run(self):
+        messages = self.dao.get_pending_messages()
+        self.logger.info(f"Encontradas {len(messages)} mensagens pendentes.")
+
+        for msg in messages:
+            sanitized_text = self.sanitizer.sanitize(msg.get("content", ""))
+            self.dao.update_message_status(msg["_id"], "sanitized", new_content=sanitized_text)
+            self.logger.info(f"Mensagem {msg['_id']} sanitizada.")
